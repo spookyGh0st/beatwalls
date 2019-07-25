1820ab11a2283a864c2cf9ebc7e22c0e3a52d621
SpookyGhost <wr3r3w6j3boa6h@mailbox.org>
2019-07-25 16:47:46 +0200

    👻changed some parameters and started the text class

--- src/main/kotlin/com/github/spookyghost/beatwalls/WallStructure.kt
+++ src/main/kotlin/com/github/spookyghost/beatwalls/WallStructure.kt
@@ -9,14 +9,33 @@ interface WallStructure {
 
     val myObstacle: ArrayList<MyObstacle>
 
-
-    fun getObstacleList(parameters: ArrayList<Double>): ArrayList<_obstacles> {
+    fun getObstacleList(pString: ArrayList<String>): ArrayList<_obstacles> {
+        val parameters = getDefaultParameter(pString)
         val list = arrayListOf<_obstacles>()
         myObstacle.forEach {
             val a = it.copy()
             a.adjust(parameters)
             list.add(a.to_obstacle())
         }
+        return list
+    }
+}
+
+
+
+
+object Text:WallStructure{
+    override val name: String = "text"
+    override val mirror: Boolean = false
+    override val myObstacle: ArrayList<MyObstacle> = arrayListOf()
+
+    override fun getObstacleList(pString: ArrayList<String>): ArrayList<_obstacles> {
+        myObstacle.clear()
+        val list = arrayListOf<_obstacles>()
+        val text = pString.subList(1,pString.lastIndex)
+        //todo transform
+        println(text)
+        myObstacle.forEach { list.add(it.to_obstacle()) }
         return list
     }
 }
@@ -47,3 +66,12 @@ data class CustomWallStructure(
 }
 
 
+private fun getDefaultParameter(parameters: ArrayList<String>): ArrayList<Double> {
+    val doubleArrayList =   arrayListOf<Double>()
+    (parameters.subList(1, parameters.lastIndex+1)).forEach {
+        doubleArrayList.add(it.toDoubleOrNull() ?: 0.0)
+    }
+    while(doubleArrayList.size<5) { doubleArrayList.add(0.0) }
+    println(doubleArrayList)
+    return doubleArrayList
+}
--- src/main/kotlin/com/github/spookyghost/beatwalls/WallStructureManager.kt
+++ src/main/kotlin/com/github/spookyghost/beatwalls/WallStructureManager.kt
@@ -6,6 +6,7 @@ object WallStructureManager
 
     fun loadManager(list:ArrayList<CustomWallStructure>) {
         with(wallStructuresList){
+            add(Text)
             addAll(list)
         }
     }
@@ -13,17 +14,8 @@ object WallStructureManager
     fun get(parameters: ArrayList<String>) =
         wallStructuresList.find {
             it.name == parameters.first().toLowerCase()
-        }?.getObstacleList(getDefaultParameter(parameters)) ?: arrayListOf()
+        }?.getObstacleList(parameters) ?: arrayListOf()
 
 
     /** The default parameter are duration - height - startHeight - startRow - width */
-    private fun getDefaultParameter(parameters: ArrayList<String>): ArrayList<Double> {
-        val doubleArrayList =   arrayListOf<Double>()
-        (parameters.subList(1, parameters.lastIndex+1)).forEach {
-            doubleArrayList.add(it.toDoubleOrNull() ?: 0.0)
-        }
-        while(doubleArrayList.size<5) { doubleArrayList.add(0.0) }
-        println(doubleArrayList)
-        return doubleArrayList
-    }
 }

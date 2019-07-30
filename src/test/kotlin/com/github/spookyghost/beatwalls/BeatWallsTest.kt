95e653a27923a8ddcd629aa53d4dac4db49f9c24
SpookyGhost <wr3r3w6j3boa6h@mailbox.org>
2019-07-30 17:47:37 +0200

    who even needs test (me not)

--- src/test/kotlin/com/github/spookyghost/beatwalls/BeatWallsTest.kt
+++ src/test/kotlin/com/github/spookyghost/beatwalls/BeatWallsTest.kt
@@ -9,9 +9,6 @@ import kotlin.test.assertEquals
 class BeatwallsTest {
 
     @Test
-    //TODO SETUP METHOD, WHICH CREATES A SAMPLE DIFFICULTY WITH ALL SPECIAL CASES
-    //TODO TESTING THE SAMPLE DIFFICULTY
-    //TODO TESTING THE MAIN METHOD
     fun testInport(){
     }
 

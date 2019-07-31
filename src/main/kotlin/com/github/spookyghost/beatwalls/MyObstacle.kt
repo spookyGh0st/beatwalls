86cd468e74e1e088ff618cea7076b3ec5fa1e17a
SpookyGhost <wr3r3w6j3boa6h@mailbox.org>
2019-07-31 15:57:20 +0200

    added the new adjustParameter, which will replace adjust

--- src/main/kotlin/com/github/spookyghost/beatwalls/MyObstacle.kt
+++ src/main/kotlin/com/github/spookyghost/beatwalls/MyObstacle.kt
@@ -14,62 +14,82 @@ data class MyObstacle(
     override fun toString(): String {
         return "duration: $duration height: $height startHeight: $startHeight startRow: $startRow width: $width StartTime: $startTime"
     }
-}
 
 
-/**Changes the MyObstacle Type to an _obstacle Type */
-fun MyObstacle.to_obstacle():_obstacles {
-    //first, so it adjust the startRow
-    val tempWidth = getWidth()
-    //other parameters
-    val tempStartTime = startTime
-    val tempLineIndex = startRow.toLineIndex()
-    val tempType = type(height,startHeight)
-    val tempDuration = duration
-    return  _obstacles(tempStartTime,tempLineIndex,tempType,tempDuration,tempWidth)
-}
+    /**Changes the MyObstacle Type to an _obstacle Type */
+    fun to_obstacle(): _obstacles {
+        //first, so it adjust the startRow
+        val tempWidth = getWidth()
+        val tempLineIndex = getLineIndex()
 
-/**overwrites the values if the parameter types are not null */
-fun MyObstacle.adjust(a:ArrayList<Double>){
-    //println("Parameters: $a")
-    //println("OLD Obstacle $this")
-
-    duration +=  a[2]
-    height += a[3]
-    startHeight += a[4]
-    startRow += a[5]
-    width += a[6]
-    startTime += a[7]
-
-
-    duration *= a[0]
-    startTime *= a[0]
-    //println("NEW Obstacle: $this")
-}
-
-/**returns the mirrored obstacle */
-fun MyObstacle.mirror()=
-    MyObstacle(duration,height,startHeight,-startRow-width,width,startTime)
-
-/**Return the _obstacle value of the startRow*/
-private fun Double.toLineIndex():Int {
-    val i = this +2
-    return if( i >= 0.0)
-        (i* 1000 +1000).toInt()
-    else
-        (i*1000-1000).toInt()
-}
-
-/**returns th _obstacle value of the width*/
-//TODO until negative width is allowed, this is kind of broken
-private fun MyObstacle.getWidth():Int{
-    return if( width >= 0.0)
-        (width* 1000 +1000).toInt()
-    else{
-        startRow -= width
-        (width*1000+1000).toInt()
+        //other parameters
+        val tempStartTime = startTime
+        val tempType = type(height, startHeight)
+        val tempDuration = duration
+        return _obstacles(
+            tempStartTime,
+            tempLineIndex,
+            tempType,
+            tempDuration,
+            tempWidth
+        )
     }
 
+
+    /**returns th _obstacle value of the width*/
+    //TODO until negative width is allowed, this is needed. Once negative width is allowed, do this like lineIndex
+    private fun getWidth():Int{
+        return if( width >= 0.0)
+            (width* 1000 +1000).toInt()
+        else{
+            startRow -= width
+            (width*1000+1000).toInt()
+        }
+    }
+
+
+    /**Return the _obstacle value of the startRow*/
+    private fun getLineIndex():Int {
+        val i = startRow +2
+        return if( i >= 0.0)
+            (i* 1000 +1000).toInt()
+        else
+            (i*1000-1000).toInt()
+    }
+
+
+    /**returns the mirrored obstacle */
+    fun mirror()=
+        MyObstacle(duration, height, startHeight, -startRow - width, width, startTime)
+
+
+    /**overwrites the values if the parameter types are not null */
+    fun adjust(a:ArrayList<Double>){
+        duration +=  a[2]
+        height += a[3]
+        startHeight += a[4]
+        startRow += a[5]
+        width += a[6]
+        startTime += a[7]
+        duration *= a[0]
+        startTime *= a[0]
+    }
+
+
+    fun adjustParameters(defaultParameters: DefaultParameters){
+
+        //Adding all the values
+        duration += defaultParameters.duration
+        height += defaultParameters.wallHeight
+        startHeight += defaultParameters.wallStartHeight
+        startRow += defaultParameters.startRow
+        width  += defaultParameters.width
+        startTime += defaultParameters.startTime
+
+        //adjusting the scale
+        duration *= defaultParameters.scale
+        startTime *= defaultParameters.scale
+    }
 }
 
 

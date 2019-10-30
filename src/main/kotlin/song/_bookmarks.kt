11e78d45845c13668a3d9273e28b466e3065e5c3
SpookyGhost <wr3r3w6j3boa6h@mailbox.org>
2019-10-30 15:00:12 +0100

    cleaned up

--- src/main/kotlin/song/_bookmarks.kt
+++ src/main/kotlin/song/_bookmarks.kt
@@ -1,28 +1,11 @@
 package song
 
 import com.google.gson.annotations.SerializedName
-import old_structures.OldParameters
-import parameter.Command
 
 data class _bookmarks (
 
     @SerializedName("_time") val _time : Double,
     @SerializedName("_name") val _name : String
 ){
-    fun getCommandList(commandName:String): ArrayList<String> {
-        val regex = """(?<=/$commandName )(.*)(?=\s/)""".toRegex()
-        val list = arrayListOf<String>()
-        val string = this._name + " /"
-        regex.findAll(string).forEach {
-            val b = it.value
-            val txt = it.value.removePrefix(commandName).removePrefix(" ")
-            list.add(txt)
-        }
-        return list
-    }
-    fun toCommandList(commandPreFix: String): List<Command> {
-        val l = getCommandList(commandPreFix)
-        return l.map { Command(_time,it) }
-    }
 }
 

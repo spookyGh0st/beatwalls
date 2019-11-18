3dff3711b432bf439894e465fe70b8da77e28ae0
SpookyGhost <wr3r3w6j3boa6h@mailbox.org>
2019-11-18 19:30:22 +0100

    added Scrip

--- src/main/kotlin/com/github/spookyghost/beatwalls/Updater.kt
+++ src/main/kotlin/com/github/spookyghost/beatwalls/Updater.kt
@@ -3,6 +3,7 @@ package com.github.spookyghost.beatwalls
 import com.google.gson.Gson
 import com.google.gson.annotations.SerializedName
 import mu.KotlinLogging
+import java.io.File
 import java.net.URL
 import java.net.UnknownHostException
 
@@ -24,12 +25,12 @@ fun update(){
     executeUpdater(s)
 }
 
-fun getLatestVersion():String{
+fun getLatestVersion(): String {
     val url =URL("https://api.github.com/repos/spookygh0st/beatwalls/releases/latest")
     return try {
         val json = url.readText()
-        val latestVersion = Gson().fromJson(json,GithubApi::class.java)
-        latestVersion.tag_name
+        val latestVersion =  Gson().fromJson(json,GithubApi::class.java)!!
+        return latestVersion.tag_name
     }catch (e: UnknownHostException){
         logger.warn { "failed to retrieve new version" }
         currentVersion
@@ -40,13 +41,40 @@ data class GithubApi (
     @SerializedName("tag_name") val tag_name : String
 )
 
-fun buildUpdater(version: String): String{
-    TODO()
+fun buildUpdater(version: String): File {
+    val os = System.getProperty("os.name")
+    val link = "https://github.com/spookyGh0st/beatwalls/releases/download/$version/beatwalls.exe"
+    val text = when (os) {
+        "Linux" -> """
+            sleep 2
+            rm beatwalls.exe
+            wget $link
+            echo "downloaded latest version"
+        """.trimIndent()
+
+        "Windows" -> """
+        Start-Sleep -s 2
+        Remove-Item beatwalls.exe
+        Import-Module BitsTransfer
+        Start-BitsTransfer -Source $link -Destination beatwalls.exe
+        .\beatwalls.exe
+        """.trimIndent()
+
+        else -> TODO()
+    }
+    val file = when(os){
+        "Linux" -> File("bwUpdater.sh")
+        "Windows" -> File("bwUpdater.ps1")
+        else -> TODO()
+    }
+    file.writeText(text)
+    file.setExecutable(true)
+    return file
 }
 
-fun executeUpdater(script: String){
+fun executeUpdater(file: File){
     TODO()
 }
 fun main(){
-    getLatestVersion()
+    buildUpdater(getLatestVersion())
 }

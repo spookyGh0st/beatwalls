032fb9edcad48dcaaaf03a6f3e94b77cae9475ab
SpookyGhost <wr3r3w6j3boa6h@mailbox.org>
2020-02-12 22:27:02 +0100

    :white_check_mark: Adding tests.

--- src/test/kotlin/structure/helperClasses/SingleColorTest.kt
+++ src/test/kotlin/structure/helperClasses/SingleColorTest.kt
@@ -3,11 +3,23 @@ package structure.helperClasses
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.Before
 
 class SingleColorTest {
+    private val l = mutableListOf<SpookyWall>()
+    @Before
+    fun createWalls() {
+        repeat(5) {
+            l.add(SpookyWall(0.0,0.0,0.0,0.0,0.0,0.0))
+        }
+    }
 
     @Test
     fun colorWalls() {
-        TODO()
+        val expected = cyan
+        SingleColor(expected).colorWalls(l)
+        l.map { it.color }.forEach{ actual ->
+            assertEquals(expected,actual)
+        }
     }
 }

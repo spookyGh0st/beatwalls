493734430aab1b22993c4dcfd492f387dd9ae4af
SpookyGhost <wr3r3w6j3boa6h@mailbox.org>
2020-02-12 23:15:07 +0100

    :bug: Fixing rgb names.

--- src/main/kotlin/chart/difficulty/_obstacleCustomData.kt
+++ src/main/kotlin/chart/difficulty/_obstacleCustomData.kt
@@ -4,7 +4,7 @@ import com.google.gson.annotations.SerializedName
 
 @Suppress("ClassName")
 data class _obstacleCustomData(
-    @SerializedName("r") val r: Double?,
-    @SerializedName("g") val g: Double?,
-    @SerializedName("b") val b: Double?
+    @SerializedName("_r") val _r: Double?,
+    @SerializedName("_g") val _g: Double?,
+    @SerializedName("_b") val _b: Double?
 )

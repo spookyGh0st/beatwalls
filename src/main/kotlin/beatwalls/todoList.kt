4eae489edf5c3fecc2464ebf38a905937ce39f18
SpookyGhost <wr3r3w6j3boa6h@mailbox.org>
2020-02-27 02:21:32 +0100

    Fix ne calculation

--- src/main/kotlin/beatwalls/todoList.kt
+++ src/main/kotlin/beatwalls/todoList.kt
@@ -3,3 +3,4 @@ package beatwalls
 // todo find a way to block a whole section for walls
 // todo make variables possible
 // todo create a loop structure
+// todo move spookywall to own package and put extension functions a seperate file and maybe rename to wall or obstacle, idc

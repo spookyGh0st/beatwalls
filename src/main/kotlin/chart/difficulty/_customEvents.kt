6089145c52ee7fa794e47e57cc8a4e4ef16488a0
SpookyGhost <wr3r3w6j3boa6h@mailbox.org>
2020-02-28 20:37:06 +0100

    Bugfix

--- src/main/kotlin/chart/difficulty/_customEvents.kt
+++ src/main/kotlin/chart/difficulty/_customEvents.kt
@@ -5,8 +5,9 @@ package chart.difficulty
 import com.google.gson.annotations.SerializedName
 
 data class _customEvents (
+    @SerializedName("_time") var _time: Float,
     @SerializedName("_type") var _type: String,
-    @SerializedName("_data") var _data : _customEventCustomData?
+    @SerializedName("_data") var _data: _customEventCustomData?
 )
 data class _customEventCustomData(
     @SerializedName("name") var name: String?,

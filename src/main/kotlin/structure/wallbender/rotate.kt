8488470a2cda2d7f4966127dfc893669f7ae682f
spookyG0st <wr3r3w6j3boa6h@mailbox.org>
2020-04-06 21:17:01 +0200

    Fix rotation offset

--- src/main/kotlin/structure/wallbender/rotate.kt
+++ src/main/kotlin/structure/wallbender/rotate.kt
@@ -6,9 +6,9 @@ import structure.helperClasses.SpookyWall
 fun WallStructure.rotate(l: List<SpookyWall>): List<SpookyWall> {
     this.rotation.rotateWalls(l)
     l.forEach{
-        it.localRotation[0]= this.localRotX()
-        it.localRotation[1]= this.localRotY()
-        it.localRotation[2]= this.localRotZ()
+        it.localRotation[0]+= this.localRotX()
+        it.localRotation[1]+= this.localRotY()
+        it.localRotation[2]+= this.localRotZ()
     }
     return l
 }
